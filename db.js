44f5e3b4f6769c5665b3b4af4effdf86896467bf
Aditya Asane <adityaasane0@gmail.com>
2026-06-17 19:26:17 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ var mysql=require('mysql2');
 var util=require('util');
 
 var conn=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'root',
-    database:'dental'
+    host:'blgmqu1rcmvpdyo9niwh-mysql.services.clever-cloud.com',
+    user:'umxfzqtnq6nkv9pg',
+    password:'fgTMnTUPugoDowpxvfcG',
+    database:'blgmqu1rcmvpdyo9niwh'
 });
 
 var exe=util.promisify(conn.query).bind(conn);
